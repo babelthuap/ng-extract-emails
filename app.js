1d340ae2a9e034daea38a2da70055c80d69c8e4f
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-25 14:00:06 -0800

    Use reduce instead of forEach

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ angular.module('emailsApp', [])
 .controller('emailsCtrl', function($scope) {
   $scope.extractEmails = function(str) {
     var arr = str ? str.match(/[A-Z\d._%+\-]+@[A-Z\d.\-]+\.[A-Z]{2,}/gi) : [];
-    var obj = {};
-    arr.forEach(function(email) {
+    return arr.reduce(function(obj, email) {
       obj[email] = obj[email] ? obj[email] + 1 : 1;
-    });
-    return obj;
+      return obj;
+    }, {});
   }
 });
