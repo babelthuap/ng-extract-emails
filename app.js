8f3b10c2f856a6431323a8f44bbcfdc392ac5fc7
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-25 14:12:40 -0800

    Never pass null to reduce

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 angular.module('emailsApp', [])
 .controller('emailsCtrl', function($scope) {
   $scope.extractEmails = function(str) {
-    var arr = str ? str.match(/[A-Z\d._%+\-]+@[A-Z\d.\-]+\.[A-Z]{2,}/gi) : [];
+    var arr = str ? str.match(/[A-Z\d._%+\-]+@[A-Z\d.\-]+\.[A-Z]{2,}/gi) || [] : [];
     return arr.reduce(function(obj, email) {
       obj[email] = obj[email] ? obj[email] + 1 : 1;
       return obj;
